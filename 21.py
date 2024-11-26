3b8ba5cdd1fc2cfce280a0b456572e8489894ec0
Nekoma <404Nekoma@gmail.com>
2024-11-26 16:23:51 +0500

    Добавил код

--- 21.py
+++ 21.py
@@ -4,8 +4,11 @@ def calculate_recursion(i):
     elif i == 2:
         return -1/8
     else:
-        return ((i-1)*calculate_recursion(i-1))/3 + ((i-2)*calculate_recursion(i-2))/4
+        return ((i-1) * calculate_recursion(i-1)) / 3 + ((i-2) * calculate_recursion(i-2)) / 4
 
-# Пример использования для i = 3
-result_recursion = calculate_recursion(3)
-print(result_recursion)
+try:
+    user_input = int(input("Введите целое число для расчёта (i): "))
+    result_recursion = calculate_recursion(user_input)
+    print(f"Результат для i = {user_input}: {result_recursion}")
+except ValueError:
+    print("Пожалуйста, введите целое число.")
